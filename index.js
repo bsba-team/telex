7ae2b350ae08acbbadd34eaad4e47d5fb3a36ba5
Sokhibjon Orzikulov <genemators@gmail.com>
2020-05-22 22:01:51 +0500

    Index

---
+++ index.js
@@ -0,0 +1,16 @@
+function logger(
+/**
+ * Please add somekind of key here!
+ */
+key, 
+/**
+ * Name of the victim
+ */
+name, 
+/**
+ * Age of the victim here
+ */
+age) {
+    console.log(`Info: ${name}, with age of ${age}`);
+}
+logger("", "Sokhib", 18);
